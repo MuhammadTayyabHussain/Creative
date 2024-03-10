1fa90db2df55a46773c4fd1837aaff3eba5c398a
mubashirbashir4179 <mubashirbashir4179@gmail.com>
2024-03-10 11:10:01 +0500

    Add project files.

---
+++ CreativeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreativeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
